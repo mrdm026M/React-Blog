8f132e18d3ceaa4f1392509e1306770e2e9f7af7
Dhruv Maheshwari <63752163+mrdm026M@users.noreply.github.com>
2021-06-29 00:15:53 +0530

    colors and other css properties added

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -1,5 +1,4 @@
 import { createGlobalStyle } from "styled-components";
-// import bgImage from "../assets/images/bg-2.jpg";
 
 const GlobalStyles = createGlobalStyle`
 *{
@@ -8,28 +7,75 @@ const GlobalStyles = createGlobalStyle`
     box-sizing: border-box;
   }
   :root{
-    --white : white;
-    --blue : #4648CA;
-    --dark-grey: #1B1B1B;
-    --grey: rgb(70,70,70);
-    --medium-grey: rgb(150,150,150);
-    --light-grey: #F6F6FC;
+    --bg-color : #f8f9fa;
+    --grey-100: #e9ecef;
+    --grey-200: #dee2e6;
+    --grey-300: #ced4da;
+    --grey-400: #adb5bd;
+    --grey-500: #6c757d;
+    --grey-600: #495057;
+    --grey-700: #343a40;
+    --grey-800: #212529;
+    --grey-900: #121519;
   }
   body{
     overscroll-behavior-y: none;
+    min-height: 100vh;
+    text-rendering: optimizeSpeed;
+    line-height: 1.5;
   }
+
   html{
-    font-family: 'MR';
+    font-family: 'RR';
     font-size: 1rem;
-    background-color: var(--white);
+    background-color: var(--bg-color);
     height: 100%;
     cursor: context-menu;
   }
-  ul,li{
-    list-style: none;
+
+  /* Set core root defaults */
+  html:focus-within {
+    scroll-behavior: smooth;
   }
-  a{
+
+  /* Remove default margin */
+  body,
+  h1,
+  h2,
+  h3,
+  h4,
+  p,
+  figure,
+  blockquote,
+  dl,
+  dd {
+    margin: 0;
+  }
+
+  /* Remove list styles on ul, ol elements with a list role, which suggests default styling will be removed */
+  ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+
+  a {
     text-decoration: none;
+  }
+
+  /* Make images easier to work with */
+  img,
+  picture {
+    max-width: 100%;
+    display: block;
+  }
+
+  /* Inherit fonts for inputs and buttons */
+  input,
+  button,
+  textarea,
+  select {
+    font: inherit;
   }
 `;
 export default GlobalStyles;
