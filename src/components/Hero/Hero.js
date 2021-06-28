6d89205b7751f9830f2324ff2c7697703d8b004f
Dhruv Maheshwari <63752163+mrdm026M@users.noreply.github.com>
2021-06-29 01:22:26 +0530

    ui done

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import blogs from "../../assets/data/blogData";
+import {} from "./Hero.scss";
+
+export const Hero = () => {
+  return (
+    <>
+      {blogs.map((blog, index) => {
+        if (blog.tags.includes("latest")) {
+          return (
+            <div
+              className="hero__section"
+              style={{
+                backgroundImage: `url(${blog.image})`,
+              }}
+            >
+              <div class="hero__section__content">
+                <article className="post__snippet">
+                  <h1 class="snippet__title">
+                    <NavLink to="/">{blog.title}</NavLink>
+                  </h1>
+                  <p class="snippet__meta">
+                    by <span>{blog.author}</span> on <span>{blog.date}</span>
+                  </p>
+                  <p class="snippet__body">{blog.description}</p>
+                  <div className="hero__section__btn">
+                    <button className="btn__primary">
+                      <NavLink to="/">Continue Reading</NavLink>
+                    </button>
+                  </div>
+                </article>
+              </div>
+            </div>
+          );
+        } else {
+          return null;
+        }
+      })}
+    </>
+  );
+};
