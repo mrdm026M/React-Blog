104195e6f8dd5b68ea4707049619fadcbd800445
Dhruv Maheshwari <63752163+mrdm026M@users.noreply.github.com>
2021-06-29 00:15:00 +0530

    ui done

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import {} from "./Navbar.scss";
+
+export const Navbar = () => {
+  return (
+    <div className="navbar__section">
+      <header className="page-navbar">
+        <div className="page-navbar__content">
+          <div className="nav-logo">
+            <NavLink to="/">Blog.</NavLink>
+          </div>
+          <nav className="nav">
+            <ul className="nav-list">
+              <li>
+                <NavLink to="/">Home</NavLink>
+              </li>
+              <li>
+                <NavLink to="/">Recent</NavLink>
+              </li>
+              <li>
+                <NavLink to="/">Categories</NavLink>
+              </li>
+              <li>
+                <NavLink to="/">Featured</NavLink>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+    </div>
+  );
+};
