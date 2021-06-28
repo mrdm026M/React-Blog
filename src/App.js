2541a5b0ffdd3fcf4239f7ed42aaa6bfaf3a5815
Dhruv Maheshwari <63752163+mrdm026M@users.noreply.github.com>
2021-06-29 00:16:21 +0530

    home route changed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Home } from "../src/pages/Home";
+import { Home } from "../src/pages/Home/Home.js";
 
 const App = () => {
   return (
     <Router>
-        <Switch>
-          <Route exact path="/" component={Home} />
-        </Switch>
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
     </Router>
   );
 };
