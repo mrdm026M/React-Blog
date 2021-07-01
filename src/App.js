1792e34b6642533562ce7cc591eb4a5536181437
Dhruv Maheshwari <63752163+mrdm026M@users.noreply.github.com>
2021-07-01 12:35:43 +0530

    added recent page route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Home } from "../src/pages/Home/Home.js";
+import { Recent } from "./pages/Recent/Recent.js";
 
 const App = () => {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/recent" component={Recent} />
       </Switch>
     </Router>
   );
