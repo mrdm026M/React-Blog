471085d0109ee51d7c93d523de0fb0beb07d997a
Dhruv Maheshwari <63752163+mrdm026M@users.noreply.github.com>
2021-07-01 12:37:22 +0530

    recent blogs section ui done

---
+++ src/components/RecentBlog/RecentBlog.js
@@ -0,0 +1,35 @@
+import React from "react";
+import blogs from "../../assets/data/blogData";
+import { Articles } from "../ArticlesPost/Articles";
+import "./RecentBlog.scss";
+
+export const RecentBlog = () => {
+  return (
+    <div className="recent-blog__section">
+      <div class="recent-blog__section-content">
+        <div className="content-header">
+          <h2 class="section-title">Recent Articles</h2>
+        </div>
+        <div className="content-articles">
+          {blogs.map((blog, index) => {
+            if (blog.tags.includes("recent")) {
+              return (
+                <Articles
+                  key={index}
+                  image={blog.image}
+                  imageAlt={blog.imageAlt}
+                  title={blog.title}
+                  author={blog.author}
+                  date={blog.date}
+                  description={blog.description}
+                />
+              );
+            } else {
+              return null;
+            }
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
