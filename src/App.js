63078306051c58fd322b3bd5238f81c7cc81e438
Dhruv Maheshwari <63752163+mrdm026M@users.noreply.github.com>
2021-07-07 12:42:41 +0530

    single blog page route added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Home } from "../src/pages/Home/Home.js";
+import ScrollToTop from "./components/StyledComponent/ScrollToTop";
+import { BlogPage } from "./pages/BlogPage/BlogPage.js";
 import { Recent } from "./pages/Recent/Recent.js";
 
 const App = () => {
   return (
     <Router>
+      <ScrollToTop />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/recent" component={Recent} />
+        <Route exact path={`/blog/:title`} component={BlogPage} />
       </Switch>
     </Router>
   );
