72850f5b93e9ced6125708dde0e1e7425933b6a4
Dhruv Maheshwari <63752163+mrdm026M@users.noreply.github.com>
2021-07-07 12:43:01 +0530

    latest post component added

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
 import blogs from "../../assets/data/blogData";
+import { LatestPost } from "../LatestPost/LatestPost";
 import {} from "./Hero.scss";
 
 export const Hero = () => {
@@ -16,24 +16,14 @@ export const Hero = () => {
                 backgroundImage: `url(${blog.image})`,
               }}
             >
-              <div className="bg-blend">
-                <div class="hero__section__content">
-                  <article className="post__snippet">
-                    <h1 class="snippet__title">
-                      <NavLink to="/">{blog.title}</NavLink>
-                    </h1>
-                    <p class="snippet__meta">
-                      by <span>{blog.author}</span> on <span>{blog.date}</span>
-                    </p>
-                    <p class="snippet__body">{blog.description}</p>
-                    <div className="hero__section__btn">
-                      <button className="btn__primary">
-                        <NavLink to="/">Continue Reading</NavLink>
-                      </button>
-                    </div>
-                  </article>
-                </div>
-              </div>
+              <LatestPost
+                img={blog.image}
+                imgAlt={blog.imageAlt}
+                title={blog.title}
+                author={blog.author}
+                date={blog.date}
+                description={blog.description}
+              />
             </div>
           );
         } else {
