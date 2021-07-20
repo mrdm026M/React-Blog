a603fb85911c8833b36ce8d902cef1fcc7e86104
Dhruv Maheshwari <63752163+mrdm026M@users.noreply.github.com>
2021-07-20 14:50:38 +0530

    firebase and firestore added

---
+++ src/Config/Fire.js
@@ -0,0 +1,17 @@
+// import "@firebase/firestore";
+import firebase from "firebase";
+// import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBLe-q7X8yUIDN1PUTQ9fkJQvuaE--YNRU",
+  authDomain: "react-blog-8fea6.firebaseapp.com",
+  projectId: "react-blog-8fea6",
+  storageBucket: "react-blog-8fea6.appspot.com",
+  messagingSenderId: "729254901736",
+  appId: "1:729254901736:web:bd7d59ad52eda772ed165d",
+};
+
+firebase.initializeApp(firebaseConfig);
+// firebase.firestore();
+
+export default firebase;
