3216f7220bb68c3c57250703de3ba454facbd941
mrdm026M <mrdm026@gmail.com>
2021-07-27 02:27:10 +0530

    route to new blog create page added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "../src/pages/Home/Home.js";
 import ScrollToTop from "./components/StyledComponent/ScrollToTop";
 import BlogPage from "./pages/BlogPage/BlogPage.js";
+import NewBlog from "./pages/NewBlog/NewBlog.js";
 import Recent from "./pages/Recent/Recent.js";
 
 const App = () => {
@@ -12,7 +13,8 @@ const App = () => {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/recent" component={Recent} />
-        <Route exact path="/blog/:title" component={BlogPage} />
+        <Route exact path="/blog/:id/:title" component={BlogPage} />
+        <Route exact path="/newblog" component={NewBlog} />
       </Switch>
     </Router>
   );
