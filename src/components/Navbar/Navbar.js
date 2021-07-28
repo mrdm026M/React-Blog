563f2201b13173f8e520d7435b6b4206035d4035
mrdm026M <mrdm026@gmail.com>
2021-07-29 02:42:40 +0530

    new blog and logout page link added

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,33 +1,67 @@
-import React from "react";
+import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
 import {} from "./Navbar.scss";
+import { connect } from "react-redux";
+import fire from "../../Config/Fire";
+// import { Button } from "react-scroll";
 
-export const Navbar = () => {
-  return (
-    <div className="navbar__section">
-      <header className="page-navbar">
-        <div className="page-navbar__content">
-          <div className="nav-logo">
-            <NavLink to="/">Blog.</NavLink>
+class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentWillReceiveProps(nextProps, nextContext) {
+    if (!nextProps.auth.isEmpty) {
+      fire
+        .auth()
+        .currentUser.getIdTokenResult()
+        .then((claim) => {
+          console.log(claim.claims);
+        });
+    }
+  }
+
+  render() {
+    return (
+      <div className="navbar__section">
+        <header className="page-navbar">
+          <div className="page-navbar__content">
+            <div className="nav-logo">
+              <NavLink to="/">Blog.</NavLink>
+            </div>
+            <nav className="nav">
+              <ul className="nav-list">
+                <li>
+                  <NavLink to="/">Home</NavLink>
+                </li>
+                <li>
+                  <NavLink to="/recent">Recent</NavLink>
+                </li>
+                {this.props.auth.isEmpty ? null : (
+                  <li>
+                    <NavLink to="/newblog">New Blog</NavLink>
+                  </li>
+                )}
+                {this.props.auth.isEmpty ? null : (
+                  <li>
+                    <NavLink to="/" onClick={() => fire.auth().signOut()}>
+                      LogOut
+                    </NavLink>
+                  </li>
+                )}
+              </ul>
+            </nav>
           </div>
-          <nav className="nav">
-            <ul className="nav-list">
-              <li>
-                <NavLink to="/">Home</NavLink>
-              </li>
-              <li>
-                <NavLink to="/recent">Recent</NavLink>
-              </li>
-              {/* <li>
-                <NavLink to="/">Categories</NavLink>
-              </li>
-              <li>
-                <NavLink to="/">Featured</NavLink>
-              </li> */}
-            </ul>
-          </nav>
-        </div>
-      </header>
-    </div>
-  );
-};
+        </header>
+      </div>
+    );
+  }
+}
+
+const enhance = connect(({ firebase: { auth, profile } }) => ({
+  auth,
+  profile,
+}));
+
+export default enhance(Navbar);
