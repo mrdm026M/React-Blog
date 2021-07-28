8ce646e2cf424bacd50f7806c51dedb196c2cd72
mrdm026M <mrdm026@gmail.com>
2021-07-29 02:41:53 +0530

    firebase redux file added

---
+++ src/Config/Fire-redux.js
@@ -0,0 +1,26 @@
+import { combineReducers, createStore } from "redux";
+import { firebaseReducer } from "react-redux-firebase";
+import firebase from "firebase";
+
+const rootReducer = combineReducers({
+  firebase: firebaseReducer,
+});
+
+const initialState = {};
+const store = createStore(rootReducer, initialState);
+
+const rrfConfig = {};
+
+const rrfProps = {
+  firebase,
+  config: rrfConfig,
+  dispatch: store.dispatch,
+};
+
+export function getReduxStore() {
+  return store;
+}
+
+export function getRrfProp() {
+  return rrfProps;
+}
