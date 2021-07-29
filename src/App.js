faaafef34808a282c6627a73b80dd85db70065d0
mrdm026M <mrdm026@gmail.com>
2021-07-29 18:10:27 +0530

    add routermanager

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "../src/pages/Home/Home.js";
+import { BrowserRouter as Router } from "react-router-dom";
 import ScrollToTop from "./components/StyledComponent/ScrollToTop";
-import BlogPage from "./pages/BlogPage/BlogPage.js";
-import LoginPage from "./pages/LoginPage/LoginPage.js";
-import NewBlog from "./pages/NewBlog/NewBlog.js";
-import Recent from "./pages/Recent/Recent.js";
 import { Provider } from "react-redux";
 import { getReduxStore, getRrfProp } from "./Config/Fire-redux";
 import { ReactReduxFirebaseProvider } from "react-redux-firebase";
+import RouterManager from "./RouterManager/RouterManager";
 
 const App = () => {
   return (
@@ -17,13 +13,7 @@ const App = () => {
         <ReactReduxFirebaseProvider {...getRrfProp()}>
           <Router>
             <ScrollToTop />
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/recent" component={Recent} />
-              <Route exact path="/blog/:id/:title" component={BlogPage} />
-              <Route exact path="/newblog" component={NewBlog} />
-              <Route exact path="/login" component={LoginPage} />
-            </Switch>
+            <RouterManager />
           </Router>
         </ReactReduxFirebaseProvider>
       </Provider>
