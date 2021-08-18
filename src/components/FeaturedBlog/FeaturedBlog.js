992276c83585a7843c87eb5a8cc0873cd11cb9e2
mrdm026M <mrdm026@gmail.com>
2021-08-18 16:59:53 +0530

    ui updated (description added)

--- src/components/FeaturedBlog/FeaturedBlog.js
+++ src/components/FeaturedBlog/FeaturedBlog.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Articles } from "../ArticlesPost/Articles";
+import { Blogs } from "../BlogsPost/Blogs";
 import "./FeaturedBlog.scss";
 
 export const FeaturedBlog = (props) => {
@@ -7,12 +7,12 @@ export const FeaturedBlog = (props) => {
     <div className="featured__section">
       <div className="featured__section-content">
         <div className="content-header">
-          <h2 className="section-title">Featured Articles</h2>
+          <h2 className="section-title">Featured Blogs</h2>
         </div>
         <div className="content-articles">
           {props.data.map((blog, index) => {
             if (blog.tags.includes("featured") && index < 4) {
-              return <Articles blog={blog} key={index} />;
+              return <Blogs blog={blog} key={index} />;
             } else {
               return null;
             }
