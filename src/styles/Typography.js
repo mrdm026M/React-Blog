c2c2a343a07ecef17b13e44b4ef1ddb7220f64de
mrdm026M <mrdm026@gmail.com>
2021-08-20 02:11:05 +0530

    some fonts removed

--- src/styles/Typography.js
+++ src/styles/Typography.js
@@ -1,15 +1,7 @@
 import { createGlobalStyle } from "styled-components";
-import CascadiaMonoBold from "../assets/fonts/CascadiaMono-Bold.ttf";
-import CascadiaMonoRegular from "../assets/fonts/CascadiaMono-Regular.ttf";
-import CascadiaMonoSemiBold from "../assets/fonts/CascadiaMono-SemiBold.ttf";
 import RobotoBold from "../assets/fonts/Roboto-Bold.ttf";
 import RobotoMedium from "../assets/fonts/Roboto-Medium.ttf";
 import RobotoRegular from "../assets/fonts/Roboto-Regular.ttf";
-
-import DB from "../assets/fonts/Dosis-Bold.ttf";
-import DM from "../assets/fonts/Dosis-Medium.ttf";
-import DR from "../assets/fonts/Dosis-Regular.ttf";
-import DSB from "../assets/fonts/Dosis-SemiBold.ttf";
 
 import OB from "../assets/fonts/Oswald-Bold.ttf";
 import OM from "../assets/fonts/Oswald-Medium.ttf";
@@ -17,21 +9,6 @@ import OR from "../assets/fonts/Oswald-Regular.ttf";
 import OSB from "../assets/fonts/Oswald-SemiBold.ttf";
 
 const Typography = createGlobalStyle`
-  @font-face {
-    font-family: 'CB';
-    src: url(${CascadiaMonoBold});
-    font-style: normal;
-  }
-  @font-face {
-    font-family: 'CSB';
-    src: url(${CascadiaMonoSemiBold});
-    font-style: normal;
-  }
-  @font-face {
-    font-family: 'CR';
-    src: url(${CascadiaMonoRegular});
-    font-style: normal;
-  }
   @font-face {
     font-family: 'RB';
     src: url(${RobotoBold});
@@ -45,27 +22,6 @@ const Typography = createGlobalStyle`
   @font-face {
     font-family: 'RR';
     src: url(${RobotoRegular});
-    font-style: normal;
-  }
-
-  @font-face {
-    font-family: 'DB';
-    src: url(${DB});
-    font-style: normal;
-  }
-  @font-face {
-    font-family: 'DM';
-    src: url(${DM});
-    font-style: normal;
-  }
-  @font-face {
-    font-family: 'DR';
-    src: url(${DR});
-    font-style: normal;
-  }
-  @font-face {
-    font-family: 'DSB';
-    src: url(${DSB});
     font-style: normal;
   }
 
